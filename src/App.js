4dbf5ff75c2a18e50dc335ad7cdc11d692ae3b73
igorshaposhnyk <igorshaposhnyk13@gmail.com>
2022-09-21 13:08:23 +0300

    init

--- src/App.js
+++ src/App.js
@@ -3,30 +3,10 @@ import './App.scss';
 import Text from "./components/Text/Text";
 
 function App() {
-
-  const arr = [12,34,"asdf","asdf"]
-
-
   return (
     <div className="App">
 
-      <Text text={"Доброе Утро!!!"} count={33} arr={arr}/>
-      <Text text="Мы з Украины!!!" tanya="Киев"/>
-
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Text />
     </div>
   );
 }
--- src/components/Text/Text.js
+++ src/components/Text/Text.js
@@ -1,35 +1,14 @@
 import React from 'react';
 import "./Text.scss"
 
-const Text = (props) => {
 
-  const {text,tanya,count,arr} = props
-
-  console.log(arr)
-
-  // const text = "Доброе Утро!!!"
-
-  const list = arr?.map((el,i)=>{
-    return <p>{i} - {el}</p>
-  })
-
-  // console.log(list)
-
-  return (
-    <p>
-
-      {list}
-
-
-      sdf
-      {/*<p className={"text"}> {list}</p>*/}
-      <div>
-
-        {/*<p>{tanya}</p>*/}
-        {/*<p>{arr}</p>*/}
-      </div>
-    </p>
-  );
-};
+function Text(){
+  return(
+    <>
+    <h1> Hello World! </h1>
+    <img src='/image/react.png'></img>
+    </>
+  )
+}
 
 export default Text;
